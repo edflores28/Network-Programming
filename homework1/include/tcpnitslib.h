4817c866d6babf6bd0604f428e122e49f49a36c2
Edwin Flores <eflores4@dev4.apl.jhu.edu>
2017-09-26 16:59:02 -0400

    Initial skeleton for the TCP socket library

---
+++ homework1/include/tcpnitslib.h
@@ -0,0 +1,9 @@
+/*
+ * Pretty header stuff goes here.
+ */
+#include <netinet/in.h>
+#define NITS_SOCKET_ERROR	(-1)
+#define NITS_SOCKET_OK		(0)
+
+int setup_subscriber (struct in_addr *host, int port);
+int setup_publisher (int port);
