d8b803a87de03b449e55f1b23c95b7edd5170d1c
Edwin Flores <edlores28@gmail.com>
2017-09-26 17:11:12 -0400

    Started writing some code

--- homework1/src/lib/unixnitslib/unixnitslib.c
+++ homework1/src/lib/unixnitslib/unixnitslib.c
@@ -1,4 +1,6 @@
+//#include <sys/socket.h>
 #include <stdio.h>
+#include "unp.h"
 #include "unixnitslib.h"
 
 int setup_subscriber(char *publisher_path)
@@ -9,6 +11,10 @@ int setup_subscriber(char *publisher_path)
 
 int setup_publisher(char *publisher_path)
 {
+	int server;
+	struct sockaddr_un server_addr;
+
+	server = socket(AF_LOCAL, SOCKS_STREAM, 0);
 	printf ("Setting up unix domain publisher on %s\n", publisher_path);
 	return (0);
 }
