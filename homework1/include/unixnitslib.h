d0ee61601ebf8ffa7bbbd5a1a88ea81da780991d
Edwin Flores <eflores4@dev4.apl.jhu.edu>
2017-09-26 16:58:33 -0400

    Initial skeleton for the UNIX domain socket library

---
+++ homework1/include/unixnitslib.h
@@ -0,0 +1,9 @@
+/*
+ * Yeah, there should be pretty header comments, here, too.
+ */
+#define NITS_SOCKET_ERROR	(-1)
+#define NITS_SOCKET_OK		(0)
+
+int setup_subscriber(char *server_path);
+
+int setup_publisher(char *server_path);
