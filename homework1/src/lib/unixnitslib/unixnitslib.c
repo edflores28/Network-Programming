657288db3e373cc4a79663c8867e3178f2ab1e25
Edwin Flores <edlores28@gmail.com>
2017-09-26 20:27:58 -0400

    Added some server code

--- homework1/src/lib/unixnitslib/unixnitslib.c
+++ homework1/src/lib/unixnitslib/unixnitslib.c
@@ -1,6 +1,6 @@
-//#include <sys/socket.h>
+#include <sys/socket.h>
 #include <stdio.h>
-#include "unp.h"
+//#include "unp.h"
 #include "unixnitslib.h"
 
 int setup_subscriber(char *publisher_path)
@@ -15,6 +15,16 @@ int setup_publisher(char *publisher_path)
 	struct sockaddr_un server_addr;
 
 	server = socket(AF_LOCAL, SOCKS_STREAM, 0);
+
+	if (server == -1)
+	{
+		perror("Error creating socket");
+		return NITS_SOCKET_ERROR;
+	}
+	memset(&server_addr, 0 sizeof(sockaddr_un));
+	strcpy(server_addr.sun_path, publisher_path);
+	server_addr.sun_family = AF_LOCAL;
+
 	printf ("Setting up unix domain publisher on %s\n", publisher_path);
-	return (0);
+	return (NITS_SOCKET_OK);
 }
