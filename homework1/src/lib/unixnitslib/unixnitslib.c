ebf4171b77d6abb37f27383c589a8f783db23408
Edwin Flores <edlores28@gmail.com>
2017-09-27 16:25:33 -0400

    Tweaks

--- homework1/src/lib/unixnitslib/unixnitslib.c
+++ homework1/src/lib/unixnitslib/unixnitslib.c
@@ -22,8 +22,10 @@ int setup_publisher(char *publisher_path)
 		perror("Error creating socket");
 		return NITS_SOCKET_ERROR;
 	}
-//	memset(&server_addr, 0 sizeof(sockaddr_un));
-//	strncpy(server_addr.sun_path, publisher_path, sizeof(server_addr.sun_path - 1));
+
+	memset(&server_addr, 0, sizeof(server_addr));
+	strncpy(server_addr.sun_path, publisher_path, sizeof(server_addr.sun_path - 1));
+	unlink(publisher_path);
 
 	server_addr.sun_family = AF_LOCAL;
 
@@ -33,9 +35,7 @@ int setup_publisher(char *publisher_path)
 		perror("Error binding");
 		return NITS_SOCKET_ERROR;
 	}
-
-
+	
 	printf ("Setting up unix domain publisher on %s\n", publisher_path);
 	return (NITS_SOCKET_OK);
 }
-
