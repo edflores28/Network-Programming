ac319c647521bbed47eaea923df008c00cf83793
Edwin Flores <eflores4@dev4.apl.jhu.edu>
2017-09-27 16:56:55 -0400

    Added functionality for the publisher, also fixed compiler warnings

--- homework1/src/lib/unixnitslib/unixnitslib.c
+++ homework1/src/lib/unixnitslib/unixnitslib.c
@@ -1,7 +1,10 @@
 #include <sys/socket.h>
 #include <stdio.h>
-//#include "unp.h"
+#include <string.h>
 #include "unixnitslib.h"
+
+// Global Variables
+static int server_fd;
 
 int setup_subscriber(char *publisher_path)
 {
@@ -11,33 +14,38 @@ int setup_subscriber(char *publisher_path)
 
 int setup_publisher(char *publisher_path)
 {
-	int server;
 	int result;
 	struct sockaddr_un server_addr;
 
-	server = socket(AF_LOCAL, SOCK_STREAM, 0);
+	// Obtain a file descriptor for the server.
+	server_fd = socket(AF_LOCAL, SOCK_STREAM, 0);
 
-	if (server == -1)
+	if (server_fd == -1)
 	{
 		perror("Error creating socket");
 		return NITS_SOCKET_ERROR;
 	}
 
+	// Clear out server_addr so it can be used.
 	memset(&server_addr, 0, sizeof(server_addr));
 
-	strncpy(server_addr.sun_path, publisher_path, sizeof(server_addr.sun_path - 1));
-	printf("input: %s, copy %s", publisher_path, server_addr.sun_path);
+	// Copy the publisher path to the server_addr path.
+	strncpy(server_addr.sun_path, publisher_path, sizeof(server_addr.sun_path) - 1);
+
+	// Unlink any instances.
 	unlink(publisher_path);
 
 	server_addr.sun_family = AF_LOCAL;
 
-	result = bind(server, (struct sockaddr*)&server_addr, sizeof(server_addr));
+	// Bind to the socket
+	result = bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+	
 	if (result == -1)
 	{
 		perror("Error binding");
 		return NITS_SOCKET_ERROR;
 	}
 
-	printf ("Setting up unix domain publisher on %s\n", publisher_path);
+	printf ("Setting up unix domain publisher eererer on %s\n", publisher_path);
 	return (NITS_SOCKET_OK);
 }
