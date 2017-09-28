9f9a2e669a58fc06c6c182bc05416bea0b666eeb
Edwin Flores <eflores4@dev4.apl.jhu.edu>
2017-09-27 20:30:20 -0400

    Fixed some bugs

--- homework1/src/lib/unixnitslib/unixnitslib.c
+++ homework1/src/lib/unixnitslib/unixnitslib.c
@@ -1,5 +1,6 @@
 #include <sys/socket.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "unixnitslib.h"
 
@@ -11,7 +12,7 @@ int setup_subscriber(char *publisher_path)
 	int listen_fd;
 	int result;
 	int i;
-	struct sockaddr client_addr;
+	struct sockaddr_un client_addr;
 
 	// Obtain a file descriptor for the server.
 	listen_fd = socket(AF_LOCAL, SOCK_STREAM, 0);
@@ -23,22 +24,22 @@ int setup_subscriber(char *publisher_path)
 	}
 
 	// Clear out server_addr so it can be used.
-	memset(&client_addr, 0, sizeof(server_addr));
+	memset(&client_addr, 0, sizeof(client_addr));
 
 	// Copy the publisher path to the server_addr path and set
 	// the socket family to local.
 	strncpy(client_addr.sun_path, publisher_path, sizeof(client_addr.sun_path) - 1);
-	server_addr.sun_family = AF_LOCAL;
+	client_addr.sun_family = AF_LOCAL;
 
 	// Loop 6 times until a connection can be established. There
 	// will be a 0.5 second delay after a failure is determined.
 	for (i = 1; i <= 6; i++)
 	{
-		result = connect(listen_fd, struct sockaddr*) &client_addr
+		result = connect(listen_fd, (struct sockaddr*) &client_addr, sizeof(client_addr));
 		if (result == -1)
 		{
 				perror("Error connecting");
-				delay(500);
+//				sleep(0.5);
 		}
 		else
 			break;
@@ -84,6 +85,15 @@ int setup_publisher(char *publisher_path)
 	if (result == -1)
 	{
 		perror("Error binding");
+		return NITS_SOCKET_ERROR;
+	}
+
+	// Listem on the socket
+	result = listen(listen_fd, 5);
+
+	if (result == -1)
+	{
+		perror("Error listening");
 		return NITS_SOCKET_ERROR;
 	}
 
