366abae6df88c13d5e4bf2ccc918302d01a28ee8
Edwin Flores <edlores28@gmail.com>
2017-10-04 17:19:49 -0400

    Added additional code

--- homework1/src/bin/unix_publisher/unix_publisher.c
+++ homework1/src/bin/unix_publisher/unix_publisher.c
@@ -5,12 +5,14 @@
 #include <stdio.h>
 #include "unixnitslib.h"
 
-int
-main(int argc, char *argv[])
+#define ARRAY_SIZE 256
+
+int main(int argc, char *argv[])
 {
-	int fd = 0;
-	char buffer[256];
-	
+	int fd;
+	int bytes;
+	char buffer[ARRAY_SIZE];
+
 	/*
 	 * check args.
 	 */
@@ -24,42 +26,50 @@ main(int argc, char *argv[])
 	 * in this example for fun.
 	 */
 
-	
+
 	if (setup_publisher (PATH) == NITS_SOCKET_ERROR)
 	{
 		fprintf (stderr, "Stupid idiot, you screwed up.\n");
 		exit(1);
 	}
 
-	// Obtain the file descriptor. Exit is there is an error.
-	fd = get_next_subscriber();
-
-	if (fd == NITS_SOCKET_ERROR)
+	while(1)
 	{
-		perror("RROR");
-		exit(1);
+			// Obtain the file descriptor. Exit is there is an error.
+			fd = get_next_subscriber();
+
+			if (fd == NITS_SOCKET_ERROR)
+			{
+				perror("Error obtaining socket");
+				exit(1);
+			}
+
+			// Read which article the subscriber requested.
+			int bytes = read(fd, buffer, ARRAY_SIZE-1);
+
+			if (bytes < 0)
+				perror("Error Reading");
+
+			// Check to see if QUIT was received,
+			// If so break from the while loop
+			if (stncmp(buffer,"QUIT",4) == 0)
+				break;
+
+
+			// Obtain a handle to the file.
+			// TODO need to search directories for the articles.
+			FILE *file;
+			file = fopen(buffer,"r");
+
+			if (file == NULL)
+			{
+				perror("Error Opening File");
+				exit(1);
+			}
+
+			while(fgets(buffer, sizeof(buffer),file))
+				write(fd,buffer,255);
 	}
-
-	// Read whcih article the publisher requested.
-	int n = read(fd, buffer, 255);
-
-	if (n < 0)
-		printf("ERROR READING");
-
-
-	// Obtain a handle to the file.
-	// TODO need to search directories for the articles.
-	FILE *file;
-	file = fopen(buffer,"r");
-
-	if (file == NULL) 
-	{
-		perror("Error Opening File");
-		exit(1);
-	}
-
-	while(fgets(buffer, sizeof(buffer),file))
-		write(fd,buffer,255);
 
 	close(fd);
 	fclose(file);
