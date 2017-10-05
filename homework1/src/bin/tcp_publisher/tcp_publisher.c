2b74e533f85c8de5d224ee685d0722818221d660
Edwin Flores <edlores28@gmail.com>
2017-10-05 18:14:39 -0400

    Ported over the unix pub/sub code for TCP

--- homework1/src/bin/tcp_publisher/tcp_publisher.c
+++ homework1/src/bin/tcp_publisher/tcp_publisher.c
@@ -3,30 +3,109 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/fcntl.h>
+#include <unistd.h>
 #include "tcpnitslib.h"
+
 #define MY_PORT	7640
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int pub_fd;
-	/*
-	 * check args.
-	 */
-	if (argc > 1)
-	{
-		fprintf (stderr, "Usage: %s\n", argv[0]);
-		exit (1);
-	}
-	/*
-	 * do your stuff here...just calling setup_publisher
-	 * in this example for fun.
-	 */
+
+	int fd;
+	int bytes;
+	char buffer[ARRAY_SIZE];
+	char article[ARRAY_SIZE];
+	FILE *file;
+	int found = -1;
+
+	char myArticle[] = "/home/eflores4/Articles/";
+	char netArticle[] = "/home/net_class/474/Articles/";
+
 	if ((pub_fd = setup_publisher (MY_PORT)) == NITS_SOCKET_ERROR)
 	{
-		fprintf (stderr, "Stupid idiot, you screwed up. You know nothing.\n");
+		fprintf (stderr, "Error setting up the publisher.\n");
 		exit(1);
 	}
 
+	while(1)
+	{
+			// Obtain the file descriptor. Exit is there is an error.
+			fd = get_next_subscriber();
+
+			if (fd == NITS_SOCKET_ERROR)
+			{
+				perror("Error obtaining socket");
+				exit(1);
+			}
+
+			memset(&buffer[0],0,sizeof(buffer));
+			// Read which article the subscriber requested.
+			bytes = read(fd, buffer, ARRAY_SIZE-1);
+
+			if (bytes < 0)
+				printf("Error Reading\n");
+
+			printf("Recieved %s from the subscriber\n", buffer);
+
+			// Check to see if QUIT was received,
+			// If so break from the while loop
+			if ((bytes == 4) && (buffer[0] == 'Q') && (buffer[1] == 'U') && (buffer[2] == 'I') && (buffer[3] == 'T'))
+			{
+				printf("QUIT received, exiting..\n");
+				break;
+			}
+
+			// Clear the article buffer and determine
+			// if the article is found in "my" path, this
+			// checks to see if the file exists and is readable.
+			memset(&article[0], 0, sizeof(article));
+			strcpy(article, myArticle);
+			strncat(article, buffer, bytes);
+
+			found = access(article,F_OK|R_OK);
+
+			// Check to see if the article was found. If not
+			// clear the article buffer and determine
+			// if the article is found in "net_class" path.
+			if (found == -1)
+			{
+				memset(&article[0], 0, sizeof(article));
+				strcpy(article, netArticle);
+				strncat(article, buffer, bytes);
+				found = access(article,F_OK|R_OK);
+			}
+
+			// If the article is found send it to the subscriber.
+			if (found == 0)
+			{
+				// Obtain a handle to the file.
+				file = fopen(article,"rb");
+
+				if (file == NULL)
+				{
+					perror("Error Opening File");
+					exit(1);
+				}
+
+				while(fgets(buffer, sizeof(buffer),file))
+					write(fd,buffer,ARRAY_SIZE-1);
+
+				printf("Finished sending to the subscriber\n");
+				close(fd);
+			}
+
+			// Close the file descriptor if the article is
+			// not found in both paths.
+			if (found != 0)
+			{
+				printf("File path: %s not found..\n",buffer);
+				close(fd);
+			}
+	}
+
+	close(fd);
+	fclose(file);
 	exit (0);
 }
--- homework1/src/bin/tcp_subscriber/tcp_subscriber.c
+++ homework1/src/bin/tcp_subscriber/tcp_subscriber.c
@@ -1,17 +1,23 @@
 /*
  *	<Header stuff goes here>
  */
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/fcntl.h>
+#include <unistd.h>
 #include "tcpnitslib.h"
+
 #define MY_PORT	7640
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	/*
-	 * check args.
-	 */
+	int fd;
+	int bytes;
+	int found;
+	char buffer[256];
+	int init_read = -1;
+	int length;
 	struct in_addr *host;
 	/*
 	 * make sure to fill in the host information correctly.
@@ -25,11 +31,58 @@ main(int argc, char *argv[])
 	 * do your stuff here...just calling setup_subscriber
 	 * in this example for fun.
 	 */
-	if (setup_subscriber (host, MY_PORT) == NITS_SOCKET_ERROR)
+	fd = setup_subscriber (host, MY_PORT);
+
+	if (fd == NITS_SOCKET_ERROR)
 	{
-		fprintf (stderr, "Stupid idiot, you screwed up. You know nothing.\n");
+		fprintf (stderr, "There was an error creating the subscriber.\n");
 		exit(1);
 	}
 
+	printf("Sending %s to the publisher\n", argv[1]);
+
+	// Send the Article that the subscriber wants from the publisher
+	// First make sure we get the size of the file.
+	for (length = 0; length < 25; length++)
+		if (argv[1][length] == 0x00)
+			break;
+
+	bytes = write(fd,argv[1],length);
+
+	// Obtain a handle to write what we receive from the publisher.
+	// Exit is there is an error.
+	FILE *file;
+	file = fopen(argv[1], "wb");
+
+	if (file == NULL)
+	{
+		perror("Unable to open and write the file");
+		close(fd);
+		exit(1);
+	}
+
+	// Read from the socket until there is is no more
+	// data available from the subscriber. Also
+	// write to the file.
+	while(bytes != 0)
+	{
+		bytes = read(fd,buffer,255);
+
+		// If nothing comes from the buffer in the initial read
+		// break from the loop, otherwhise do not break.
+		if (init_read == -1)
+			if (bytes == 0)
+				break;
+			else
+				init_read = 0;
+
+		fputs(buffer, file);
+	}
+
+	if (init_read == -1)
+		printf("There was nothing recieved from the publisher\n");
+
+	close(fd);
+	fclose(file);
 	exit (0);
 }
