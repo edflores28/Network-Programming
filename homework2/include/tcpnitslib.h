131e32657185a79f05cd79b629a2db77f26eaeca
Edwin Flores <edlores28@gmail.com>
2017-10-19 18:41:46 -0400

    Added discovery messages

--- homework2/include/tcpnitslib.h
+++ homework2/include/tcpnitslib.h
@@ -26,12 +26,44 @@
 #define NITS_SOCKET_OK		(0)
 #define PUBLISHER_PORT		(8404)
 #define DISCOVERY_PORT		(8404)
-
 /*
  * Discovery service handles this many publishers
 */
 #define NUM_PUBLISHERS 5
 
+typedef enum {GET_PUB_LIST, PUB_LIST, ADVERTISE} DISCOVERY_MESSAGES;
+
+/*
+ * Message sent from subscriber to discovery service
+ */
+
+typedef struct {
+	DISCOVERY_MESSAGES  msg_type;	/* GET_PUB_LIST */
+} disc_get_pub_list;
+
+/*
+ * Message sent from discovery service to subscriber
+ */
+typedef struct {
+	DISCOVERY_MESSAGES  msg_type;	/* PUB_LIST */
+	int num_publishers;
+	struct sockaddr_in	address[NUM_PUBLISHERS];
+} disc_pub_list;
+
+/*
+ * Message sent from publisher to discovery service
+ */
+typedef struct {
+	DISCOVERY_MESSAGES  msg_type;		/* ADVERTISE */
+	int pubaddr_size;			/* size of address */
+	struct sockaddr_in	pub_address;	/* Address of publisher */
+} disc_advertise;
+
+typedef union {
+	disc_get_pub_list getlist;
+	disc_pub_list publist;
+	disc_advertise putpub;
+} discovery_msgs;
 
 int setup_subscriber (struct in_addr *host, int port);
 
--- homework2/include/unixnitslib.h
+++ homework2/include/unixnitslib.h
@@ -25,7 +25,45 @@
 
 #define PATH "/home/eflores4/serversocket"
 
-#define DISCOVERY_PATH	"/home/eflores4/disc"
+#define DISCOVERY_PATH	"/tmp/port.8404_d"
+/*
+ * Discovery service handles this many publishers
+*/
+#define NUM_PUBLISHERS 5
+
+typedef enum {GET_PUB_LIST, PUB_LIST, ADVERTISE} DISCOVERY_MESSAGES;
+
+/*
+ * Message sent from subscriber to discovery service
+ */
+
+typedef struct {
+	DISCOVERY_MESSAGES  msg_type;	/* GET_PUB_LIST */
+} disc_get_pub_list;
+
+/*
+ * Message sent from discovery service to subscriber
+ */
+typedef struct {
+	DISCOVERY_MESSAGES  msg_type;	/* PUB_LIST */
+	int num_publishers;
+	struct sockaddr_un	address[NUM_PUBLISHERS];
+} disc_pub_list;
+
+/*
+ * Message sent from publisher to discovery service
+ */
+typedef struct {
+	DISCOVERY_MESSAGES  msg_type;		/* ADVERTISE */
+	int pubaddr_size;			/* size of address */
+	struct sockaddr_un	pub_address;	/* Address of publisher */
+} disc_advertise;
+
+typedef union {
+	disc_get_pub_list getlist;
+	disc_pub_list publist;
+	disc_advertise putpub;
+} discovery_msgs;
 
 int setup_subscriber(char *server_path);
 
