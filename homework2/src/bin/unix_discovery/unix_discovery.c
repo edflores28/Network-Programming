9cafe49db1efe8cdfecd049e5cfc9966e24efa97
Edwin Flores <eflores4@jhu.edu>
2017-10-19 18:57:44 -0400

    Fixed compilation issues

--- homework2/src/bin/unix_discovery/unix_discovery.c
+++ homework2/src/bin/unix_discovery/unix_discovery.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
   disc_advertise pub_mesg;
   disc_pub_list disc_mesg;
 
-  struct sockaddr_un pub_addresses[NUM_PUBLISHERS];
+  struct sockaddr_un pub_addrs[NUM_PUBLISHERS];
   int total_pubs = 0;
 
   fd = setup_discovery_server(DISCOVERY_PATH);
@@ -45,7 +45,7 @@ int main(int argc, char *argv[])
       // store it into the list and increment the counter
       if ((pub_mesg.msg_type == ADVERTISE) && total_pubs < NUM_PUBLISHERS)
       {
-        pub_addresses[total_pubs] = pub_mesg;
+        pub_addrs[total_pubs] = pub_mesg.pub_address;
         total_pubs++;
 
         memset(&pub_mesg, 0, sizeof(pub_mesg));
@@ -62,20 +62,19 @@ int main(int argc, char *argv[])
         // if it was requested.
         if (pub_mesg.msg_type == GET_PUB_LIST)
         {
-          disc_mesg.mesg_type = PUB_LIST;
+          disc_mesg.msg_type = PUB_LIST;
           disc_mesg.num_publishers = total_pubs;
-          disc_mesg.address = pub_addresses;
+          
+	  memcpy(&disc_mesg.address[0], &pub_addrs, sizeof(disc_mesg.address));
 
-          sendto(fd, &pub_mesg, sizeof(pub_mesg), 0, (struct sockaddr *)&client, &size);
+          sendto(fd, &pub_mesg, sizeof(pub_mesg), 0, (struct sockaddr *)&client, sizeof(client));
 
           // Clear the message
           memset(&sub_mesg, 0, sizeof(sub_mesg));
         }
     }
 
-  }
-
     // Clear the buffer
     memset(&buffer, 0, sizeof(buffer));
-  }
+ }
 }
